820d919c8c3484204d03d1e40a047bee3cce0326
GunalDSP <gunaldeivegan@gmail.com>
2025-05-27 21:50:12 +0530

    Initial commit with Terraform config

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo Hello from Atlantis"
+  }
+}
+
+resource "null_resource" "second" {
+  provisioner "local-exec" {
+    command = "echo This is another test"
+  }
+}
